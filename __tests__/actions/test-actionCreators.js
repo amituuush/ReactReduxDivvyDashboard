2cfa084b205885b8237802596995ce5417a48b21
Bryan Purcell <purcebr@gmail.com>
2017-01-11 02:02:31 -0800

    refactor and test the async station fetch

--- __tests__/actions/test-actionCreators.js
+++ __tests__/actions/test-actionCreators.js
@@ -1,7 +1,52 @@
 import { fetchNearbyStations, fetchUserLocation } from '../../src/actionCreators'
+import nock from 'nock'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk';
 
-describe("actions", () => {
-  it("should create an action to check for nearby bike share stations.", () => {
+import {
+
+  NEARBY_STATIONS_REQUEST,
+  NEARBY_STATIONS_SUCCESS,
+  NEARBY_STATIONS_ERROR,
+
+  USER_LOCATION_REQUEST,
+  USER_LOCATION_SUCCESS,
+  USER_LOCATION_ERROR,
+
+} from '../../src/actionTypes';
+
+const mockStore = configureMockStore([thunk])
+
+describe("Async fetch station details from the server.", () => {
+
+  afterEach( () => {
+    nock.cleanAll();
+  });
+
+  it("Dispatches FETCH_STATIONS_SUCCESS when the station data is successfully retrieved.", () => {
+
+    // Set up nock with the example data.
+
+    nock('http://shrouded-beach-2183.herokuapp.com')
+      .get('/stations/nearby?lat=37.774929&lon=-122.419416&max_stations=5').reply(200, []);
+
+    // create a mock store
+
+    const store = mockStore({ todos: [] })
+
+    // Verify the expected actions were fired, in the correct order.
+
+    const expectedActions = [
+      { type: NEARBY_STATIONS_REQUEST },
+      { type: NEARBY_STATIONS_SUCCESS, stations: [] }
+    ]
+
+    return store.dispatch(fetchNearbyStations())
+      .then(() => { // return of async actions
+        expect(store.getActions()).toEqual(expectedActions)
+      })
+
+
 
   });
 });
--- src/actionCreators/index.js
+++ src/actionCreators/index.js
@@ -1,41 +1,106 @@
 import axios from 'axios';
-import createApiURL from '../helpers'
+import { createApiURL } from '../helpers'
+import fetch from 'isomorphic-fetch';
+
 import {
-  FETCH_NEARBY_STATIONS,
-  FETCH_USER_LOCATION
+
+  NEARBY_STATIONS_REQUEST,
+  NEARBY_STATIONS_SUCCESS,
+  NEARBY_STATIONS_ERROR,
+
+  USER_LOCATION_REQUEST,
+  USER_LOCATION_SUCCESS,
+  USER_LOCATION_ERROR,
+
 } from '../actionTypes';
 
 export const fetchNearbyStations = (lat = 37.774929, lon = -122.419416, maxStations = 5) => {
 
-  var request = axios.get(createApiURL(lat, lon, maxStations));
+  return dispatch => {
 
-  return {
-    type: FETCH_NEARBY_STATIONS,
-    payload: request
+    dispatch(fetchNearbyStationsFetching());
+
+    return fetch(createApiURL(lat, lon, maxStations))
+      .then(function(response) {
+        return response.json();
+      } )
+      .then(function(json) {
+        console.log(json);
+          dispatch(fetchNearbyStationsSuccess(json))
+        }
+      )
+      .catch(ex => dispatch(fetchNearbyStationsError(ex)))
   }
+}
+
+const fetchNearbyStationsFetching = () => {
+  return ({
+    type: 'NEARBY_STATIONS_REQUEST'
+  });
+}
+
+const fetchNearbyStationsSuccess = (stations) => {
+  return {
+    type: NEARBY_STATIONS_SUCCESS,
+    stations
+  };
+}
+
+const fetchNearbyStationsError = (exception) => {
+  return {
+    type: NEARBY_STATIONS_ERROR,
+    exception
+  };
 }
 
 export const fetchUserLocation = () => {
-  if(navigator.location) {
-    var successCallback = (position) => {
+  return dispatch => {
+    if(navigator.location) {
 
-      let { longitude, latitude } = position.coords;
+      dispatch(userLocationFetching());
 
-      return {
-        type: FETCH_USER_LOCATION,
-        longitude,
-        latitude
+      var successCallback = (position) => {
+
+        let { longitude, latitude } = position.coords;
+
+        return {
+          type: FETCH_USER_LOCATION,
+          longitude,
+          latitude
+        }
       }
-    }
 
-    var errorCallback = () => {
-      return {
-        type: FETCH_USER_LOCATION,
-        longitude: -87,
-        latitude: 41
+      var errorCallback = () => {
+        return {
+          type: FETCH_USER_LOCATION,
+          longitude: -87,
+          latitude: 41
+        }
       }
-    }
 
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    }
   }
 }
+
+const userLocationFetching = () => {
+  return {
+    type: USER_LOCATION_REQUEST
+  };
+}
+
+const userLocationSuccess = (stations) => {
+  return {
+    type: USER_LOCATION_SUCCESS,
+    stations
+  };
+}
+
+const userLocationError = (exception) => {
+  return {
+    type: USER_LOCATION_ERROR,
+    exception
+  };
+}
+
+
