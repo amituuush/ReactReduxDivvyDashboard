d519346048187a561471cdabb7a9491af9740df5
Amit Ranan <ranan.amit@gmail.com>
2017-01-12 19:11:34 -0800

    install material-ui and include MuiThemeProvider component in Root

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import configureStore from '../configureStore'
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from '../configureStore';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-import App from './App'
+import App from './App';
 
-export default () => { 
+export default () => {
   return (
     <Provider store={configureStore()}>
-      <App />
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <App />
+      </MuiThemeProvider>
     </Provider>
   )
 }
