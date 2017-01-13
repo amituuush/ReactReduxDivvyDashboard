38a07af85b4ca52252c2780bae247880b5d58436
Amit Ranan <ranan.amit@gmail.com>
2017-01-12 21:28:01 -0800

    fixed text-align style issue

--- src/components/StationExcerpt.js
+++ src/components/StationExcerpt.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { TableRow, TableRowColumn } from 'material-ui/Table'
+import '../index.css'
 
 const StationExcerpt = (props) => {
 
@@ -8,10 +9,26 @@ const StationExcerpt = (props) => {
 
   return (
     <TableRow className="station-excerpt">
-      <TableRowColumn className="station-excerpt__name">{stationName}</TableRowColumn>
-      <TableRowColumn className="station-excerpt__bikes">{availableBikes}</TableRowColumn>
-      <TableRowColumn className="station-excerpt__docks">{availableDocks}</TableRowColumn>
-      <TableRowColumn className="station-excerpt__total">{totalBikes}</TableRowColumn>
+      <TableRowColumn
+        className="station-excerpt__name"
+        style={{ textAlign: 'center'}} >
+          {stationName}
+      </TableRowColumn>
+      <TableRowColumn
+        className="station-excerpt__bikes"
+        style={{ textAlign: 'center'}} >
+          {availableBikes}
+      </TableRowColumn>
+      <TableRowColumn
+        className="station-excerpt__docks"
+        style={{ textAlign: 'center'}} >
+          {availableDocks}
+      </TableRowColumn>
+      <TableRowColumn
+        className="station-excerpt__total"
+        style={{ textAlign: 'center'}} >
+          {totalBikes}
+      </TableRowColumn>
     </TableRow>
   )
 }
